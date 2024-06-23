825dbf2b06064cec7704bb60bf13960fb4792a89
Ayush Kumar <kumarayush0427@gmail.com>
2024-06-23 17:14:17 +0530

    remove categories

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,14 +4,14 @@ import axios from "axios"
 import { Link } from "react-router-dom";
 
 export default function Sidebar() {
-  const [cats, setCats] = useState([]);
-  useEffect(() => {
-    const getCats = async () => {
-      const res = await axios.get("/categories");
-      setCats(res.data);
-    }
-    getCats();
-  }, []);
+  // const [cats, setCats] = useState([]);
+  // useEffect(() => {
+  //   const getCats = async () => {
+  //     const res = await axios.get("/categories");
+  //     setCats(res.data);
+  //   }
+  //   getCats();
+  // }, []);
   return (
     <div className="sidebar">
       <div className="sidebarItem">
@@ -25,8 +25,8 @@ export default function Sidebar() {
         </p>
       </div>
       <div className="sidebarItem">
-        <span className="sidebarTitle">CATEGORIES</span>
-        <ul className="sidebarList">
+        {/* <span className="sidebarTitle">CATEGORIES</span> */}
+        {/* <ul className="sidebarList">
           {cats?.map((c) => (
             <Link to={`/?cat=${c.name}`} className="link">
               <li className="sidebarListItem">{c.name}</li>
@@ -34,7 +34,7 @@ export default function Sidebar() {
           ))}
 
 
-        </ul>
+        </ul> */}
       </div>
       <div className="sidebarItem">
         <span className="sidebarTitle">FOLLOW US</span>
