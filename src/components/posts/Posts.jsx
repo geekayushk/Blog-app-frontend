2cbee1ff49aedcd3d830e9ff2737a77913e88b25
Ayush Kumar <kumarayush0427@gmail.com>
2024-06-23 16:53:43 +0530

    undefined fixed

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,12 +1,12 @@
 import Post from "../post/Post"
 import "./posts.css"
 
-export default function Posts({posts}) {
+export default function Posts({ posts }) {
   console.log(posts);
   return (
     <div className="posts">
-      {posts.map((p)=>(
-        <Post post={p} key={p._id}/>
+      {posts && posts.map((p) => (
+        <Post post={p} key={p._id} />
       ))}
     </div>
   )
